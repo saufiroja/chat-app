99a6b1d6b3835972578ee9f76a07ed1edf2d25fe
saufiroja <saufiroja08@gmail.com>
2021-10-10 22:58:03 +0800

    membuat user dan room chat

---
+++ utils/users.js
@@ -0,0 +1,36 @@
+const users = [];
+
+// join user to chat
+const userJoin = (id, username, room) => {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+};
+
+// Get current user
+const currentUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+// user leave chat
+const userLeave = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+// get room user
+const getRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = {
+  userJoin,
+  currentUser,
+  userLeave,
+  getRoom,
+};
